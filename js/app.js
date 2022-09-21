3de470948547827b936ee19a83229514c4a5822d
Debriks <deborah.robbiano@gmail.com>
2022-09-21 19:51:06 +0200

    HTML Structure / Styling / Media Queries

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 const btn = document.querySelector("#btn");
 
 const emailWarning = document.querySelector("#email");
-const warning = document.querySelector(".hero-info-submit-warning");
-const iconWarning = document.querySelector("#warningIcon");
+const warning = document.querySelector(".submit__warning");
+const warningIcon = document.querySelector("#warningIcon");
 
 const show = () => {
-  iconWarning.classList.add("visible");
-  warning.classList.add("visible");
   emailWarning.classList.add("visible");
+  warning.classList.add("visible");
+  warningIcon.classList.add("visible");
 };
 
 const hide = () => {
-  iconWarning.classList.remove("visible");
-  warning.classList.remove("visible");
   emailWarning.classList.remove("visible");
+  warning.classList.remove("visible");
+  warningIcon.classList.remove("visible");
 };
 
 const re =
